f5fa0b291bf74c028e6cc5422205f62f8f79cc11
mux <freelancer.c@gmail.com>
2014-02-09 22:35:27 +0200

    Increase CDC CMD EP size

--- stm/stmusbd/usbd_conf.h
+++ stm/stmusbd/usbd_conf.h
@@ -9,7 +9,7 @@
 #define CDC_OUT_EP                  0x01  /* EP1 for data OUT */
 #define CDC_CMD_EP                  0x82  /* EP2 for CDC commands */
 #define CDC_DATA_MAX_PACKET_SIZE    64   /* Endpoint IN & OUT Packet size */
-#define CDC_CMD_PACKET_SZE          8    /* Control Endpoint Packet size */
+#define CDC_CMD_PACKET_SZE          64    /* Control Endpoint Packet size */
 #define CDC_IN_FRAME_INTERVAL       5    /* Number of frames between IN transfers */
 #define APP_RX_DATA_SIZE            2048 /* Total size of IN buffer: 
                                                 APP_RX_DATA_SIZE*8/MAX_BAUDARATE*1000 should be > CDC_IN_FRAME_INTERVAL */
