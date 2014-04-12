adbad3df855d23315cb672da500277f216f02433
mux <freelancer.c@gmail.com>
2014-04-12 18:56:04 +0200

    Reduce RX buffer size

--- stm/stmusbd/usbd_conf.h
+++ stm/stmusbd/usbd_conf.h
@@ -11,7 +11,7 @@
 #define CDC_DATA_MAX_PACKET_SIZE    64   /* Endpoint IN & OUT Packet size */
 #define CDC_CMD_PACKET_SZE          64    /* Control Endpoint Packet size */
 #define CDC_IN_FRAME_INTERVAL       5    /* Number of frames between IN transfers */
-#define APP_RX_DATA_SIZE            2048 /* Total size of IN buffer: 
+#define APP_RX_DATA_SIZE            1024 /* Total size of IN buffer: 
                                                 APP_RX_DATA_SIZE*8/MAX_BAUDARATE*1000 should be > CDC_IN_FRAME_INTERVAL */
 
 // MSC parameters
